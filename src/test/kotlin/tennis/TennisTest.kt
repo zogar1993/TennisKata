d0b26adc4813a4f0ace62dbec4046194c7427f1a
Ragoz <zogar1993@hotmail.com>
2020-06-02 01:22:43 -0300

    fifteen-love

--- src/test/kotlin/tennis/TennisTest.kt
+++ src/test/kotlin/tennis/TennisTest.kt
@@ -11,8 +11,42 @@ class TennisTest {
         game.score.shouldBeEqualTo("love-love")
     }
 
+    @Test
+    fun `the game being 'love-love', player 1 scores, game is 'fifteen-love'`(){
+        val game = Game.new()
+
+        game.scoreForPlayer(1)
+
+        game.score.shouldBeEqualTo("fifteen-love")
+    }
+
+    @Test
+    fun `the game being 'love-love', player 2 scores, game is 'love-fifteen'`(){
+        val game = Game.new()
+
+        game.scoreForPlayer(2)
+
+        game.score.shouldBeEqualTo("love-fifteen")
+    }
+
     class Game private constructor() {
-        var score = "love-love"
+        private var player1Score = 0
+        fun scoreForPlayer(player: Int) {
+            player1Score++
+        }
+
+        val score: String get() {
+            return "${scoreToText(player1Score)}-love"
+        }
+
+        private fun scoreToText(score: Int): String {
+            return when (score) {
+                0 -> "love"
+                1 -> "fifteen"
+                else -> throw NotImplementedError()
+            }
+        }
+
         companion object {
             fun new() = Game()
         }
