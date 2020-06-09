6d6636c00d20dd99376f30ab40a91d769cd550a8
Ragoz <zogar1993@hotmail.com>
2020-06-09 01:18:41 -0300

    Implemented with a Finite state machine.

--- src/main/kotlin/tennis/Game.kt
+++ src/main/kotlin/tennis/Game.kt
@@ -1,62 +1,55 @@
 package tennis
 
-import kotlin.math.abs
-
 class Game private constructor() {
-    private var player1Score = 0
-    private var player2Score = 0
+    private var _score: Score = SimpleScore()
 
     fun player1Scores() {
-        if (gameAlreadyFinished) throw GameAlreadyFinished()
-        player1Score++
+        _score = _score.player1Scores()
     }
 
     fun player2Scores() {
-        if (gameAlreadyFinished) throw GameAlreadyFinished()
-        player2Score++
+        _score = _score.player2Scores()
     }
 
-    val score: String get() {
-        if (atLeast3PointsWhereScoredByEachPlayer){
-            return when {
-                playerScoresAreEqual -> "deuce"
-                player1ScoreIsOneMoreThanHisOpponents -> "advantage for player 1"
-                player2ScoreIsOneMoreThanHisOpponents -> "advantage for player 2"
-                player1ScoreIsTwoMoreThanHisOpponents -> "player 1 won the game"
-                player2ScoreIsTwoMoreThanHisOpponents -> "player 2 won the game"
-                else -> throw NotImplementedError()
-            }
-        }
+    val score: String get()  = _score.score()
 
-        if (player1Score == 4) return "player 1 won the game"
-        if (player2Score == 4) return "player 2 won the game"
+    companion object {
+        fun new() = Game()
+    }
+}
 
+
+
+interface Score {
+    fun player1Scores(): Score
+    fun player2Scores(): Score
+    fun score(): String
+}
+
+class SimpleScore(
+        private val player1Score: Int = 0,
+        private val player2Score: Int = 0): Score {
+    override fun player1Scores(): Score {
+        val player1Score = player1Score + 1
+        if (player1Score == 4 && player2Score < 3) return Player1WonScore
+        if (player1Score == 3 && player2Score == 3) return DeuceScore
+        if (player1Score == 4 && player2Score == 3) return AdvantagePlayer1Score
+
+        return SimpleScore(player1Score, player2Score)
+    }
+
+    override fun player2Scores(): Score {
+        val player2Score = player2Score + 1
+        if (player2Score == 4 && player1Score < 3) return Player2WonScore
+        if (player2Score == 3 && player1Score == 3) return DeuceScore
+        if (player2Score == 4 && player1Score == 3) return AdvantagePlayer2Score
+
+        return SimpleScore(player1Score, player2Score)
+    }
+
+    override fun score(): String {
         return "${scoreToText(player1Score)}-${scoreToText(player2Score)}"
     }
-
-    private val player1ScoreIsOneMoreThanHisOpponents get() =
-        player1Score == player2Score + 1
-
-    private val player2ScoreIsOneMoreThanHisOpponents get() =
-        player2Score == player1Score + 1
-
-    private val player1ScoreIsTwoMoreThanHisOpponents get() =
-        player1Score == player2Score + 2
-
-    private val player2ScoreIsTwoMoreThanHisOpponents get() =
-        player2Score == player1Score + 2
-
-    private val playerScoresAreEqual get() =
-        player1Score == player2Score
-
-    private val atLeast3PointsWhereScoredByEachPlayer get() =
-        player1Score >= 3 && player2Score >= 3
-
-    private val gameAlreadyFinished get() =
-        if (atLeast3PointsWhereScoredByEachPlayer)
-            abs(player1Score - player2Score) == 2
-        else player1Score == 4 || player2Score == 4
-
 
     private fun scoreToText(score: Int): String {
         return when (score) {
@@ -67,8 +60,34 @@ class Game private constructor() {
             else -> throw NotImplementedError()
         }
     }
+}
 
-    companion object {
-        fun new() = Game()
-    }
+object Player1WonScore: Score {
+    override fun player1Scores(): Score = throw GameAlreadyFinished()
+    override fun player2Scores(): Score = throw GameAlreadyFinished()
+    override fun score(): String = "player 1 won the game"
+}
+
+object Player2WonScore: Score {
+    override fun player1Scores(): Score = throw GameAlreadyFinished()
+    override fun player2Scores(): Score = throw GameAlreadyFinished()
+    override fun score(): String = "player 2 won the game"
+}
+
+object DeuceScore: Score {
+    override fun player1Scores(): Score = AdvantagePlayer1Score
+    override fun player2Scores(): Score = AdvantagePlayer2Score
+    override fun score(): String = "deuce"
+}
+
+object AdvantagePlayer1Score: Score {
+    override fun player1Scores(): Score = Player1WonScore
+    override fun player2Scores(): Score = DeuceScore
+    override fun score(): String = "advantage for player 1"
+}
+
+object AdvantagePlayer2Score: Score {
+    override fun player1Scores(): Score = DeuceScore
+    override fun player2Scores(): Score = Player2WonScore
+    override fun score(): String = "advantage for player 2"
 }
