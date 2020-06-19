48c11b1607b92e9e0b417c4b18566f1d9f94a8e4
Ragoz <zogar1993@hotmail.com>
2020-06-19 01:20:15 -0300

    Finished CircularPrimes.kt

--- src/test/kotlin/circular_primes/CircularPrimesTest.kt
+++ src/test/kotlin/circular_primes/CircularPrimesTest.kt
@@ -5,8 +5,8 @@ import org.junit.Test
 class CircularPrimesTest {
     @Test
     fun `prime numbers`(){
-        val circularPrimes = arrayOf(2, 3, 5, 7, 11)
-        val notCircularPrimes = arrayOf(1, 4, 6, 8, 9, 10, 12)
+        val circularPrimes = arrayOf(2, 3, 5, 7, 11, 13, 17, 37, 79, 113, 197, 199, 337, 1193, 3779, 11939)
+        val notCircularPrimes = arrayOf(1, 4, 6, 8, 9, 10, 12, 15, 21, 27, 51, 89)
         circularPrimes.forEach { assert(isCircularPrime(it)) { "$it should be a circular prime" } }
         notCircularPrimes.forEach { assert(!isCircularPrime(it)) { "$it should not be a circular prime" } }
     }
@@ -14,9 +14,22 @@ class CircularPrimesTest {
     fun isCircularPrime(value: Int): Boolean {
         if (value == 1) return false
         if (value == 2) return true
-        if (value % 2 == 0) return false
+        if (value.isEven()) return false
+        val values = valueMutations(value)
         for (i in 3 until value step 2)
-            if (value % i == 0) return false
+            if (values.any { it % i == 0 })
+                return false
         return true
     }
+
+    private fun valueMutations(value: Int): List<Int> {
+        val string = value.toString()
+        return (0 until string.length).map {
+            val firstPart = string.substring(0, it)
+            val secondPart = string.substring(it, string.length)
+            "$secondPart$firstPart".toInt()
+        }
+    }
+
+    private fun Int.isEven() = this % 2 == 0
 }
